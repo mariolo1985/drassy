1024b92bdacbfe0ec5c4f11b9f7ca6f56ff331c8
Mario Lo <kinfonglo@overstock.com>
2018-09-02 19:22:33 -0600

    selected state class

--- src/components/Menu/MenuItem.js
+++ src/components/Menu/MenuItem.js
@@ -1,13 +1,25 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 const MenuItem = ({
     name,
     tagline,
     url
 }) => {
+    const addSelectClass = () => {
+        const pathName = window.location.pathname;
+        if ((pathName === '/') && (url.indexOf('/overstock.html') === 0)) {
+            return true;
+        }
+
+        if ((pathName !== '/') && url.indexOf(pathName) === 0) {
+            return true;
+        }
+
+        return false;
+    };
+
     return (
-        <div className='menu-item' >
+        <div className={`menu-item${addSelectClass() ? ' selected' : ''}`}>
             <a href={url} className='menu-link'>
                 <div className='menu-item-name'>{name}</div>
                 <div className='menu-item-tagline'>{tagline}</div>
